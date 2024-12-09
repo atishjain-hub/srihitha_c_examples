28c864197d019cb1b90cde8d8310f05c78f4ef40
Atish <atishkumarjain@gmail.com>
2024-12-09 08:19:07 +0530

    First Program

---
+++ 001_hello.c
@@ -0,0 +1,17 @@
+/*
+Program name: 001_hello.c
+Task: This Program is to Display Hello Message
+Date: 9th Dec 2024
+Author: Atish Jain
+*/
+
+# include <stdio.h>
+int main()//starting point of the program execution
+{
+  printf("Welcome to C Programming");
+  printf("\nC is Very Powerful Programming Language");
+
+  getch(); //wait for a key
+  printf("\n");
+  return 0;
+}
