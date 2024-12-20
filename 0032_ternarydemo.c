ac6a027435daf71de1b002107dfe6e720e87e8da
Atish Jain <atish_jain@yahoo.com>
2024-12-20 07:42:02 +0530

    Add files via upload

---
+++ 0032_ternarydemo.c
@@ -0,0 +1,12 @@
+//Program to work with Ternary Operator
+
+# include<stdio.h>
+int main()
+{
+    10>5 ? printf("Green") : print("Red");
+    printf("\n");
+
+    10*0 ? printf("Welcome") : printf("Good Bye");
+
+    return 0;
+}
